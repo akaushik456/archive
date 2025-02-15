6c4d326968219971bae8db743c97978fd43addce
Aman Kaushik <kaushikaman239@gmail.com>
2025-02-15 17:10:20 +0530

    add-cursor-follower

--- script/script.js
+++ script/script.js
@@ -60,3 +60,27 @@ $(document).ready(function(){
     });
   });
 
+
+
+// Cursor Follower
+const cursor = document.querySelector("#cursor");
+const cursorBorder = document.querySelector("#cursor-border");
+const cursorPos = { x: 0, y: 0 };
+const cursorBorderPos = { x: 0, y: 0 };
+
+document.addEventListener("mousemove", (e) => {
+  cursorPos.x = e.clientX;
+  cursorPos.y = e.clientY;
+
+  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
+
+requestAnimationFrame(function loop() {
+  const easing = 8; // Fixed spelling mistake from "easting" to "easing"
+  cursorBorderPos.x += (cursorPos.x - cursorBorderPos.x) / easing;
+  cursorBorderPos.y += (cursorPos.y - cursorBorderPos.y) / easing;
+
+  cursorBorder.style.transform = `translate(${cursorBorderPos.x}px, ${cursorBorderPos.y}px)`;
+  
+  requestAnimationFrame(loop);
+});
